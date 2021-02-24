91b97f3818e3038c91f16fc91651f5f0b0e43057
anudeep-17 <54953858+anudeep-17@users.noreply.github.com>
2021-02-25 00:27:18 +0530

    check 2

--- src/lexicalanalysis/lexer.java
+++ src/lexicalanalysis/lexer.java
@@ -1,7 +1,6 @@
 package lexicalanalysis;
 
 import java.util.List;
-import java.util.HashMap;
 import java.util.*;
 
 
@@ -17,12 +16,13 @@ public class lexer
 	{
 		List<token> result = new ArrayList<token>();
 	
-		/*String[] str = new String[100];
+		String[] str = new String[100];
+		
+		HashMap<String, TokenType> knownwords = new HashMap<String ,TokenType>();
+		   knownwords.put("print" , TokenType.print);
+
 		
 		// this is used to alter input so that any way of entering the expression dosent alter the desired input
-		
-		
-		
 		
 		if(input.contains("-"))
 		{
@@ -45,106 +45,140 @@ public class lexer
 		 
 		}
 		if(input.contains(""))
-		if(input.contains("="))
-		{
-			input = input.replaceAll("\\=", "= ");//adds extra space for identification of the lexer
-		}	
-		if(input.contains("<"))
-		{
-			input = input.replaceAll("\\<", " <");//adds extra space for identification of the lexer
-		 
-		}
-		//if(input.contains(")"))
-		//{
-		//	input = input.replaceAll("\\)", " ) ");//adds extra space for identification of the lexer
-		 
-		//}
+			if(input.contains("="))
+			{
+				input = input.replaceAll("\\=", "= ");//adds extra space for identification of the lexer
+			}	
+			if(input.contains("<"))
+			{
+				input = input.replaceAll("\\<", " <");//adds extra space for identification of the lexer
+			 
+			}
+			//if(input.contains(")"))
+			//{
+			//	input = input.replaceAll("\\)", " ) ");//adds extra space for identification of the lexer
+			 
+			//}
+			
+		   //we slit based of space if exists in the expression if that has
+			if(input.contains(" ") && !(input.contains("\"")))
+			{
+				
+			str = input.split(" ");
+			}
+			else if(input.contains(" ") && (input.contains("\"")))
+			{
+				
+			str = input.split("\"");
+			}
+		
+			
+			//if it dosent have a space in between then we split on symbol and digit basis.
+			else
+			{	
+			//	str = input.split("(?<=[- + * / %(),])(?=.)|(?<=.)(?=[- + * / %(),])");
+				str = input.split("(?<=[\\d.])(?=[^\\d.])|(?<=[^\\d.])(?=[\\d.])|(?=[()])|(?<=[()])");
+				//System.out.println(Arrays.toString(str));
+			}
+
+
 		
 	   //we slit based of space if exists in the expression if that has
-		if(input.contains(" ") && !(input.startsWith("\"")))
-		{
-			
-		str = input.split(" ");
-		}
 		
 		//if it dosent have a space in between then we split on symbol and digit basis.
-		else
-		{	
+		//else
+		//{	
 		//	str = input.split("(?<=[- + * / %(),])(?=.)|(?<=.)(?=[- + * / %(),])");
-			str = input.split("(?<=[\\d.])(?=[^\\d.])|(?<=[^\\d.])(?=[\\d.])|(?=[()])|(?<=[()])");
-		}
-		*/
-		
-		HashMap<String, TokenType> knownwords = new HashMap<String ,TokenType>();
-		   knownwords.put("print" , TokenType.print);
 
+		//}
 		
 		//System.out.println(Arrays.toString(str));
 		//starting of state machine
 		int state = 0;//initial value of state machine
 		
-		for(int i = 0 ; i < input.length(); i++)
+		for(int i = 0 ; i < str.length; i++)
 		{
-			//each index of the string is taken and used to compare its equivalency
-			
-	        char s = input.charAt(i);
-			
-			if(Character.isDigit(s))//checks if it is number 
+			String s = str[i];//each index of the string is taken and used to compare its equivalency
+			if(isNumeric(s))//checks if it is number 
 			{
-				
 				state = 1;
 			}
 			
-			if(s == '+')//checks for + and sets appropriate value of state
+			if(s.equalsIgnoreCase("+"))//checks for + and sets appropriate value of state
 			{
 				state = 2;
 			}
-			if(s == '-')//checks for - and sets appropriate value of state
+			if(s.equalsIgnoreCase("-"))//checks for - and sets appropriate value of state
 			{
 				
 				state = 3;
-				
 			}
-			if(s == '*')//checks for * and sets appropriate value of state
+			if(s.equalsIgnoreCase("*"))//checks for * and sets appropriate value of state
 			{
 				state = 4;
 			}
-			if(s == '/')//checks for / and sets appropriate value of state
+			if(s.equalsIgnoreCase("/"))//checks for / and sets appropriate value of state
 			{
 				state = 5;
 			}
-			
-			if((s == ' ')|| (s == '\t')) //checks for the space , tab or empty so that to address that case
+			if(s.equalsIgnoreCase(" ") || s.equalsIgnoreCase("\t") || s.isEmpty()) //checks for the space , tab or empty so that to address that case
 			{
 				state = 6;
 			}
-			if(s == '=')
+			if(s.equalsIgnoreCase("="))//checks for / and sets appropriate value of state
 			{
 				state = 7;
 			}
-			if(s == '<')
+			if(s.equalsIgnoreCase("<"))//checks for / and sets appropriate value of state
 			{
 				state = 8;
 			}
-			if(s == '>')
+			if(s.equalsIgnoreCase(">"))//checks for / and sets appropriate value of state
 			{
 				state = 9;
 			}
-			if(s == '(')
+			if(s.equalsIgnoreCase("<="))//checks for / and sets appropriate value of state
+			{
+				state = 10;
+			}
+			if(s.equalsIgnoreCase(">="))//checks for / and sets appropriate value of state
+			{
+				state = 11;
+			}
+			if(s.equalsIgnoreCase("<>"))//checks for / and sets appropriate value of state
+			{
+				state = 12;
+			}
+			if(s.equalsIgnoreCase("("))//checks for / and sets appropriate value of state
 			{
 				state = 13;
 			}
-			if(s == ')')
+			if(s.equalsIgnoreCase(")"))//checks for / and sets appropriate value of state
 			{
 				state = 14;
 			}
-			
+			if(s.matches(".*[a-zA-Z]+.*") && (!(s.contains("print")) || !(s.contains("PRINT"))) && !(s.contains("\"")))
+			{
+				state = 15;
+			}
+			if(s.matches(".*[a-zA-Z]+.*") && ((s.contains("print")) || (s.contains("PRINT"))) && !(s.contains("\"")))
+			{
+				state = 16;
+			}
+			if(s.matches(".*[a-zA-Z]+.*") && s.contains(":"))
+			{
+				state = 17;
+			}
+			if(s.matches(".*[a-zA-Z]+.*") && s.contains(":"))
+			{
+				state = 17;
+			}
 		
 			
 			switch(state)
 			{
-			case 1 : result.add(new token(TokenType.number , ""));break;//adds number to the token list using enum reference
-			case 2 : result.add(new token(TokenType.plus, "+"));break;//adds plus to the token list using enum reference
+			case 1: result.add(new token(TokenType.number , s));break;//adds number to the token list using enum reference
+			case 2: result.add(new token(TokenType.plus, "+"));break;//adds plus to the token list using enum reference
 			case 3 : result.add(new token(TokenType.minus, "-"));break;//adds minus to the token list using enum reference
 			case 4 : result.add(new token(TokenType.times, "*"));break;//adds multiply to the token list using enum reference
 			case 5 : result.add(new token(TokenType.divide, "/"));break;//adds divide to the token list using enum reference
@@ -157,27 +191,21 @@ public class lexer
 			case 12 :result.add(new token(TokenType.notequal, "<>"));break;
 			case 13 :result.add(new token(TokenType.lparen, "("));break;
 			case 14 :result.add(new token(TokenType.rparen, ")"));break;
-			case 15: result.add(new token(TokenType.identifier, ""));break;
-			case 16: result.add(new token(TokenType.print, ""));break;
-			case 17 : result.add(new token(TokenType.label,""));break;
-			case 18: result.add(new token(TokenType.string,""));break;
-				
+			case 15: result.add(new token(TokenType.identifier, s));break;
+			case 16: result.add(new token(TokenType.print, s));break;
+			case 17 : result.add(new token(TokenType.label,s));break;
+			case 18 : result.add(new token(TokenType.string,s));break;
+
 			default: System.out.print("***improper input please correct****");//default case if a non recognized char is entered
 			}
 			
 		}
 		result.add(new token(TokenType.endofline, " "));
 		return result;
-	}}
+	}
 	
-	
-	
-
-	
-	
-/*	
 //used to identify is it a num or not
-	public static boolean isNumeric(char strNum) 
+	public static boolean isNumeric(String strNum) 
 	{
 	    if (strNum == null) //if string is null it gives false
 	    {
@@ -195,4 +223,3 @@ public class lexer
 
 
 }
-*/
